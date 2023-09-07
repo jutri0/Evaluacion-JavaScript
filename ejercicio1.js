175d2a81514233dc182eae93114c968b17479c6c
Jutri0 <juliand_triana@soy.sena.edu.co>
2023-09-07 08:27:42 -0500

    Doc: Primer ejercicio

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+let empleados = [
+    { nombre:'raul', sueldo:1500},
+    { nombre:'maria', sueldo:2500},
+    { nombre:'miguel', sueldo:900},
+    { nombre:'laura', sueldo:900},
+    { nombre:'francisco', sueldo:900}
+];
+let promedio= empleados.sueldo;
+if (empleados.sueldo>=1000)
+{ console.log(empleados.nombre);}
