1040b54aae52e3018d7f36085f44318d1a6cdc9f
Jutri0 <juliand_triana@soy.sena.edu.co>
2023-09-07 08:40:49 -0500

    Doc: Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,8 @@
+let sueldoa=parseInt(prompt('Ingrese su sueldo'));
+
+let bono=(sueldoa*0.4);
+let anual=(sueldoa*12);
+let calculars=total=(anual+bono);
+
+console.log('su bono es: ' +bono);
+console.log('Su salario anual es: '+total);
